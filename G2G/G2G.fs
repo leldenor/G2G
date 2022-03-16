145b57205909e96e5ec1dcdb67bd1afb1d7b9c32
Work <Work@DESKTOP-K0L1SM5>
2022-03-16 09:09:29 +0100

    Add project files.

---
+++ G2G/G2G.fs
@@ -0,0 +1,40 @@
+﻿module G2G
+
+type Size = Small | Medium | Large
+
+type DrinkType = Coffee | Tea | Juice
+
+type Drink = {Type : DrinkType; Size : Size; Price: float}
+
+type Product = {ProductType : Drink; Amount : float}
+
+type VIAPerson = {Name : string}
+type SOSUPerson = {Name : string}
+type Customer = VIAPerson | SOSUPerson
+
+//type CreditCard = {Number:int}
+//type Cash = {Amount: int}
+//type MobilePay = {}
+type Payment = CreditCard | Cash | MobilePay
+
+type Order = {ProductList: Product[]; Payment: Payment; Customer: Customer}
+
+let Latte = {Type = Coffee; Size = Small; Price = 15.00}
+let Pepermint = {Type = Tea; Size = Medium; Price = 10.00}
+let Apple = {Type = Juice; Size = Large; Price = 25.00}
+
+let product = {ProductType = Latte; Amount = 2.0}
+let ProductList = [Latte; Latte; Apple]
+let sumProd = product.ProductType.Price * product.Amount
+let sum = Latte.Price + Pepermint.Price
+
+let rec calculateProducts lst =
+    let rec calculateHelper (lst, total) = // total = accumulator value
+        match lst with
+            | [] -> total
+            | hd::tl -> 
+                let ntotal = hd.Price + total
+                calculateHelper(tl, ntotal)
+    calculateHelper(lst, 0)
+    
+
