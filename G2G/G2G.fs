5dfd409b71f6100fadbf874a4ae5054941d447a4
leldenor <norenberga19@outlook.lv>
2022-03-24 09:38:44 +0100

    final without comment

--- G2G/G2G.fs
+++ G2G/G2G.fs
@@ -8,60 +8,60 @@ type Drink = {Type : DrinkType; Size : Size; Price: float}
 
 type Product = {ProductType : Drink; Amount : float}
 
-type VIAPerson = {Name : string}
-type SOSUPerson = {Name : string}
 type Customer = VIAPerson | SOSUPerson
-
-//type CreditCard = {Number:int}
-//type Cash = {Amount: int}
-//type MobilePay = {}
-
 
 type Payment = CreditCard | Cash | MobilePay
 
-type Order = {ProductList: Product[]; Payment: Payment; Customer: Customer}
+type Order = {ProductList: Product list; Payment: Payment; Customer: Customer}
 
-let Latte = {Type = Coffee; Size = Small; Price = 15.00}
-let Pepermint = {Type = Tea; Size = Medium; Price = 10.00}
-let Apple = {Type = Juice; Size = Large; Price = 25.00}
+let green = {Type = Tea; Size = Medium; Price = 15.00}
+let black = {Type = Tea; Size = Small; Price = 10.00}
+let fruit = {Type = Tea; Size = Large; Price = 20.00}
 
-let product = {ProductType = Latte; Amount = 2.0}
-let ProductList = [Latte; Latte; Apple]
-let sumProd = product.ProductType.Price * product.Amount
-let sum = Latte.Price + Pepermint.Price
+let latte = {Type = Coffee; Size = Medium; Price = 15.00}
+let cappuccino = {Type = Coffee; Size = Small; Price = 10.00}
+let mocha = {Type = Coffee; Size = Large; Price = 20.00}
 
-let rec calculateProducts lst =
-    let rec calculateHelper (lst, total) = // total = accumulator value
-        match lst with
-            | [] -> total
-            | hd::tl -> 
-                let ntotal = hd.Price + total
-                calculateHelper(tl, ntotal)
-    calculateHelper(lst, 0)
+let apple = {Type = Juice; Size = Medium; Price = 15.00}
+let orange = {Type = Juice; Size = Small; Price = 10.00}
+let grape = {Type = Juice; Size = Large; Price = 20.00}
 
 /// ------------------------------------------------------------------------- ///
 
-let OrderMsgSystem =
-    MailboxProcessor<Order>.Start(fun inbox ->
-        let rec processMessage state = 
-            async {
-                let! msg = inbox.Recieve()
-                printfn "recieved a order!"
-                let rec calculateProducts msg =
-                    let rec calculateHelper (msg, total) =
-                        match lst with
-                            | [] -> total
-                            | hd::tl -> 
-                                match hd.Type with
-                                    | DrinkType.Coffee -> 
-                                        fun 
-                                    | _ ->
-                                        let ntotal = hd.Price + total
-                                        calculateHelper(tl, ntotal)
-                    calculateHelper(msg, 0)          
-                printfn "processed the order! Please pay %f DKK" total     
-                            }
-                            processMessage "initialState")
 
-// I dont get 
-let gtgVat n x = x * (1 + n/100)
+let gtgVat n x = x * (1.0 + n/100.0)
+
+let rec calculateProducts lst =
+    let rec calculateHelper (lst, total) = 
+        match lst with
+            | [] -> total
+            | hd::tl ->
+                match hd.ProductType.Type with
+                    | Coffee -> 
+                        let x = (hd.ProductType.Price + (gtgVat hd.ProductType.Price 2.5)) * hd.Amount + total
+                        calculateHelper(tl, x)
+                    | _ ->
+                       let ntotal = hd.ProductType.Price * hd.Amount + total
+                       calculateHelper(tl, ntotal)
+    calculateHelper(lst, 0)
+
+
+let OrderMsgSystem =
+    MailboxProcessor.Start(fun inbox ->
+        let rec processMessage = 
+            async {
+                let! msg = inbox.Receive()
+                printfn "recieved a order!"
+                let newMsg = calculateProducts msg.ProductList
+                printfn "processed the order! Please pay %f DKK" newMsg
+                return! processMessage 
+            }
+        processMessage)
+
+let product1 = {ProductType = green; Amount = 2}
+let product2 = {ProductType = apple; Amount = 10}
+OrderMsgSystem.Post({ProductList = [product2]; Payment = Cash; Customer = VIAPerson})
+
+
+
+
